370f263243c872b82d0875066495d62cb76a3e94
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-26 21:26:59 +0300

    source code added

---
+++ src/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import logo from './media/logo.png'
+
+class Info extends Component {
+    render() {
+        return(
+            <div className="about">
+                <a className='code' target="_blank" title="Play Sudoku" href="https://github.com/harunsasmaz/play-sudoku"></a>
+            </div>
+        )
+    }
+}
+export default Info
